8a52d9e8f82a660c8abb06345fe99dcb3650211f
agrawaljay12 <agrawaljay247@gmail.com>
2026-09-25 09:32:29 +0530

    refactor: reorganize CORS middleware and improve origin handling

--- backend/app/index.js
+++ backend/app/index.js
@@ -1,25 +1,31 @@
 import express from "express";
-const app = express();
-import router from "../routes/index.js"
 import cors from "cors";
+import router from "../routes/index.js";
 
-//middleware 
+const app = express();
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://mern-stack-weld-nu.vercel.app",
 ];
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    exposedHeaders: ["X-Guest-Token"],
+  })
+);
 
-app.use(cors({ 
-    origin: allowedOrigins,
-    exposedHeaders: ["X-Guest-Token"]
- }));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(morgan("dev"))
 
-// register routes of application
 router(app);
 
 export default app;
